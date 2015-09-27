07d44e4806aebd5c0fa3ea3d9e31c0b11271ce2b
arminhenres <henres.armin@live.de>
2015-09-27 14:01:14 +0200

    Added RoboMessage

--- BeerBot/RobotControl/MoveMasterRobot.cs
+++ BeerBot/RobotControl/MoveMasterRobot.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,11 @@ namespace RobotControl
 {
     public class MoveMasterRobot
     {
+        private IBus _connection;
         public MoveMasterRobot()
         {
-
+            
         }
+
     }
 }
