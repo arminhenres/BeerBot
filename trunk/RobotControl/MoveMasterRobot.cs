7a1e613e511a1f506f5b89ccac264baf89954ead
arminhenres <henres.armin@live.de>
2015-12-16 11:09:08 +0100

    Commando-Liste + Abarbeitung implementiert, IsInitialized-Status, isBusy-Status, Speichern der Commando-Liste in XML-File implementiert

--- trunk/RobotControl/MoveMasterRobot.cs
+++ trunk/RobotControl/MoveMasterRobot.cs
@@ -16,11 +16,17 @@ namespace RobotControl
         private SerialPort _connection;
 
         private List<string> commands;
+
+        private bool _isInitialized;
+
+        private bool _isBusy;
         #endregion
 
         #region Constructor
         public MoveMasterRobot()
         {
+            _isBusy = false;
+            _isInitialized = false;
             _connection = new SerialPort();
             _connection.PortName = "COM1";
             _connection.DataBits = 7;
@@ -30,7 +36,7 @@ namespace RobotControl
             _connection.StopBits = StopBits.Two;
             _connection.RtsEnable = true;
             _connection.Handshake = Handshake.RequestToSend;
-            commands = new List<string>();
+            
         }
         #endregion
 
@@ -45,12 +51,14 @@ namespace RobotControl
         /// </summary>
         public void Init()
         {
+            commands = new List<string>();
             _connection.DataReceived += new SerialDataReceivedEventHandler(this.OnDatateReceived);
             if (!_connection.IsOpen)
             {
                 _connection.Open();
             }
-            Nest();
+            Nest(true);
+            _isInitialized = true;
         }
 
         /// <summary>
@@ -58,12 +66,14 @@ namespace RobotControl
         /// </summary>
         public void Deinit()
         {
+            commands = null;
+            Nest(true);
             _connection.DataReceived -= new SerialDataReceivedEventHandler(this.OnDatateReceived);
             if (_connection.IsOpen)
             {
                 _connection.Close();
             }
-            Nest();
+            _isInitialized = false;
         }
 
         /// <summary>
@@ -74,9 +84,9 @@ namespace RobotControl
         /// <param name="z">z coordinate</param>
         /// <param name="l1">l1 coordinate</param>
         /// <param name="l2">l2 coordinate</param>
-        public void MoveToPosition(double x, double y, double z, double l1, double l2)
+        public void MoveToPosition(double x, double y, double z, double l1, double l2, bool instant)
         {
-            SendMessage("MP " + x.ToString() + ", " + y.ToString() + ", " + z.ToString() + ", " + l1.ToString() + ", " + l2.ToString());
+            SendMessage("MP " + x.ToString() + ", " + y.ToString() + ", " + z.ToString() + ", " + l1.ToString() + ", " + l2.ToString(), instant);
         }
 
         /// <summary>
@@ -87,10 +97,13 @@ namespace RobotControl
         /// <param name="j3">angle 3</param>
         /// <param name="j4">angle 4</param>
         /// <param name="j5">angle 5</param>
-        public void MoveJoint(decimal j1, decimal j2, decimal j3, decimal j4, decimal j5, int speed)
+        public void MoveJoint(decimal j1, decimal j2, decimal j3, decimal j4, decimal j5, int speed, bool instant)
         {
-            Speed(speed);
-            SendMessage("MJ " + j1.ToString() + ", " + j2.ToString() + ", " + j3.ToString() + ", " + j4.ToString() + ", " + j5.ToString());
+            string message = "MJ " + j1.ToString() + ", " + j2.ToString() + ", " + j3.ToString() + ", " + j4.ToString() + ", " + j5.ToString();
+            Speed(speed, instant);
+            SendMessage(message, instant);
+            
+            
         }
 
         /// <summary>
@@ -98,44 +111,56 @@ namespace RobotControl
         /// </summary>
         public void Reset()
         {
-            SendMessage("RS");
+            SendMessage("RS", true);
         }
 
         /// <summary>
         /// Moves to Nest Position
         /// </summary>
-        public void Nest()
+        public void Nest(bool instant)
         {
-            SendMessage("NT");
+            SendMessage("NT", instant);
         }
 
         /// <summary>
         /// Moves to Origin Position
         /// </summary>
-        public void Origin()
+        public void Origin(bool instant)
         {
-            SendMessage("OG");
+            SendMessage("OG", instant);
         }
 
         /// <summary>
         /// Sets the Speed
         /// </summary>
         /// <param name="speed">Speed value to set, must be between 1 and 7</param>
-        public void Speed(int speed)
+        public void Speed(int speed, bool instant)
         {
             if(speed > 0 && speed < 8)
             {
-                SendMessage("SP " + speed.ToString());
+                SendMessage("SP " + speed.ToString(), instant);
             }
             else
             {
-                SendMessage("SP 5");
+                SendMessage("SP 5", instant);
             }
             
         }
         public void SaveFile(string path)
         {
             XmlWriter writer = XmlWriter.Create(path);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Pipapo");
+            int i = 0;
+            foreach(string s in commands)
+            {
+                writer.WriteElementString("Befehl_"+i.ToString(), s);
+                i++;
+            }
+            
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Flush();
            
         }
 
@@ -144,17 +169,27 @@ namespace RobotControl
         /// Sends Message
         /// </summary>
         /// <param name="message"></param>
-        private void SendMessage(string message)
+        private void SendMessage(string message, bool instant)
         {
-            _connection.Write(message + "\r\n"); 
+            if(instant)
+            {
+                _connection.Write(message + "\r\n");
+            }
+            else
+            {
+                commands.Add(message + "\r\n");                
+            }
+           
         }
 
-        private void ExecuteCommands()
+        public void ExecuteCommands()
         {
+            _isBusy = true;
             foreach(string command in commands)
             {
-                SendMessage(command);
+                _connection.Write(command);
             }
+            _isBusy = false;
         }
         #endregion
     }
