b8cbbec8ce71b5bad3601f0e97f5073179667244
arminhenres <henres.armin@live.de>
2016-03-02 19:37:33 +0100

    Unit Tests hinzugefügt

---
+++ trunk/RVM1Testing/MoveMasterRobotTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RobotControl;
+
+namespace RVM1Testing
+{
+    [TestClass]
+    public class MoveMasterRobotTests
+    {
+        MoveMasterRobot _robot = new MoveMasterRobot();
+        [TestMethod]
+        [ExpectedException(typeof(IOException),
+    "A userId of null was inappropriately allowed.")]
+        public void Pipapo()
+        {
+            _robot.Init();
+        }
+    }
+}
