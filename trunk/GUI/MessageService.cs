e75e9bd5de22856f7478aa6867e60f68f6e2f9a5
arminhenres <henres.armin@live.de>
2016-03-17 10:05:48 +0100

    MessageService hinzugefügt

---
+++ trunk/GUI/MessageService.cs
@@ -0,0 +1,22 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GUI
+{
+    public class MessageService
+    {
+        public static async Task<MessageDialogResult> ShowMessage(string title, string message, MessageDialogStyle dialogStyle)
+        {
+            var metroWindow = (Application.Current.MainWindow as MetroWindow);
+            metroWindow.MetroDialogOptions.ColorScheme = MetroDialogColorScheme.Accented;
+
+            return await metroWindow.ShowMessageAsync(title, message, dialogStyle, metroWindow.MetroDialogOptions);
+        }
+    }
+}
